5ce08a40d6311bd2b9c7fa96bf0966b86ba527da
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-05-17 15:25:15 -0300

    Adicionado Model user, migration para user e validação de obrigatorieadade de senha e email

---
+++ db/migrate/20210517182018_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email, null: false
+      t.string :password, null: false
+
+      t.timestamps
+    end
+  end
+end
