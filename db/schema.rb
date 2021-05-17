dd478465fc1a3f2c95604976eb674b2c81100ca6
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-05-17 17:58:56 -0300

    Mudança na Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_182018) do
+ActiveRecord::Schema.define(version: 2021_05_17_204757) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
