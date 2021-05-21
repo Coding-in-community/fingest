1ea001e98db650d8f2151cb406d5bb9cab97a1b2
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-05-21 15:05:16 -0300

    Adicionado validações em Models/User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+
+  validates :first_name, :last_name,:email, :password, presence: true
+  validates :email, uniqueness: {message: 'an account associated with %{value} already exists '}
+  validates :password, length: {:in 8..32} 
 end
